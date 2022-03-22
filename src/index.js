ea41f933c5e0aff3380e1688bb69dc722886dcc8
Pete McNeely <pete.mcneely@gmail.com>
2022-03-22 10:01:43 -0400

    let firebase own the routing

--- src/index.js
+++ src/index.js
@@ -4,19 +4,11 @@ import { createServer } from 'http';
 var fileServer = new(Server)('public', { cache: 7200 });
 
 createServer((request, response) => {
-  fileServer.serve(request, response, (err, res) => {
-    if (err && err.status === 404) {
-      if (request.url.toLowerCase() === '/resume.html' || request.url.toLowerCase() === '/resume') {
-        fileServer.serveFile('/Resume.pdf', 200, {}, request, response);
-      }
-      else if (request.url.toLowerCase() === '/index') {
-        fileServer.serveFile('/index.html', 200, {}, request, response);
-      }
-      else {
-        fileServer.serveFile('/404.html', 404, {}, request, response);
-      }
-    } else if (err && err.status === 500) {
-      fileServer.serveFile('/500.html', 500, {}, request, response);
+  fileServer.serve(request, response, (err, _) => {
+    if (err) {
+      console.error(err);
     }
   });
 }).listen(8080);
+
+console.log("Starting website");
